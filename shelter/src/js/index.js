8436dd6c980e86bcf9282250ba4acb899f3e1b27
Alina M <crlin.m@gmail.com>
2024-09-02 18:18:36 +0500

    feat: create random first 3 pet cards js script, start popup

--- shelter/src/js/index.js
+++ shelter/src/js/index.js
@@ -2,6 +2,10 @@ const BUTTON_LEFT = document.querySelector(".left-arrow");
 const BUTTON_RIGHT = document.querySelector(".right-arrow");
 
 const PETS_LIST_MAIN = document.querySelector(".friends-slider-list")
+const petsCard = document.querySelector(".our-friends-slider-card")
+const popupCard = document.querySelector(".pop-up-card");
+const body = document.querySelector("body");
+// const widthPetsList = document.querySelector(".friends-slider-list").clientWidth;
 
 async function getPets(){
     const res = await fetch("src/js/pets.json");
@@ -10,7 +14,7 @@ async function getPets(){
         const cardPets = await res.json();
         return cardPets;
     } else {
-        console.log("error");
+        throw "failed fetch pets.json";
     }
 }
 
@@ -38,13 +42,81 @@ function createPet(pet){
     PETS_LIST_MAIN.append(card);
 }
 
+function randomIndex(minInd, maxInd){
+    return Math.round(Math.random()*(maxInd - minInd) + minInd);
+}
+
+function generatePetsOrder(cntCards, cntPets) {
+    let tempArray = Array.from({length: cntPets}, (v, i) => i);
+    console.log(tempArray);
+
+    let minInd = 0;
+    const maxInd = cntPets-1;
+
+    while (minInd < cntCards){
+        let randomInd = randomIndex(minInd, maxInd);
+        console.log('randomInd', randomInd);
+        const temp = tempArray[minInd];
+        tempArray[minInd] = tempArray[randomInd];
+        tempArray[randomInd] = temp;
+        minInd++;
+    }
+    console.log('tempArray res: ', tempArray, tempArray.slice(0, 3), minInd, maxInd)
+    return tempArray;
+}
+
 async function addRandomPet(){
     const pets = await getPets();
-    createPet(pets[3]);
-    createPet(pets[4]);
-    createPet(pets[5]);
+    const cntPets = pets.length;
+    const cntCards = 3;
+    let orderArray = generatePetsOrder(cntCards, cntPets);
+    console.log(orderArray)
+    for (let i of orderArray.slice(0, cntCards)){
+        createPet(pets[i]);
+    }
+}
 
+async function newSliderPets(){
+    await addRandomPet();
     PETS_LIST_MAIN.classList.add("transition-right");
 }
 
-BUTTON_RIGHT.addEventListener("click", addRandomPet);
+function getPetDescription(event){
+    console.log(event, event.target, event.target.classList, event.target.parentNode);
+    if (event.target.classList.contains("our-friends-slider-card") ||
+        event.target.parentNode.classList.contains("our-friends-slider-card")){
+        popupCard.classList.toggle("hidden");
+    }
+}
+
+function popupClose(event){
+    // body.classList.toggle("shadow");
+    // console.log(body.clientHeight, body.offsetHeight)
+    // if (event.target.classList.contains("pop-up-card") ||
+    //     event.target.parentNode.classList.contains("pop-up-card")){
+    if (event.target.classList.contains("pop-up-card")){
+        popupCard.classList.toggle("hidden");
+        body.classList.toggle("shadow");
+    }
+}   
+
+async function init(){
+    await addRandomPet();
+}
+
+init();
+
+BUTTON_RIGHT.addEventListener("click", newSliderPets);
+PETS_LIST_MAIN.addEventListener("animationend", () => {
+    PETS_LIST_MAIN.classList.remove("transition-right");
+})
+
+petsCard.addEventListener("click", (e) => {
+    popupCard.classList.toggle("hidden");
+    body.classList.toggle("shadow");
+    // console.log('generatePetsOrder', generatePetsOrder());
+});
+
+popupCard.addEventListener("click", popupClose);
+
+
