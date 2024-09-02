828cffc51a7ec8203bc54b99f718dfae15cb2e00
Alina M <crlin.m@gmail.com>
2024-09-03 02:37:27 +0500

    refactor: creating popup by js into separate function

--- shelter/src/js/index.js
+++ shelter/src/js/index.js
@@ -18,6 +18,65 @@ async function getPets(){
     }
 }
 
+function fillPopup(pet){
+    popupCard.classList.toggle("hidden");
+    
+    const popupContent = document.querySelector('.pop-up__content');
+    popupContent.textContent = '';
+    console.log(popupCard)
+
+    const imgPopup = document.createElement('img');
+    imgPopup.alt = '';
+    imgPopup.src = pet.img;
+
+    const descriptionPet = document.createElement('div');
+    descriptionPet.classList.add("description-pet");
+
+    const petTitle = document.createElement('h3');
+    petTitle.classList.add('pet-title');
+    petTitle.textContent = pet.name;
+
+    const pBreed = document.createElement('p');
+    pBreed.classList.add('breed');
+    pBreed.textContent = pet.type + " - " + pet.breed;
+
+    const pDescription = document.createElement('p');
+    pDescription.classList.add('description');
+    pDescription.textContent = pet.description;
+
+
+    const ulDesc = document.createElement("ul");
+    
+    const liAge = document.createElement("li");
+    const bAge = document.createElement("b");
+    bAge.textContent = 'Age: ';
+    liAge.append(bAge, pet.age);
+
+    const liInoculations = document.createElement("li");
+    const bInoculations = document.createElement("b");
+    bInoculations.textContent = 'Inoculations: ';
+    liInoculations.append(bInoculations, pet.inoculations);
+
+    const liDiseases = document.createElement("li");
+    const bDiseases = document.createElement("b");
+    bDiseases.textContent = 'Diseases: ';
+    liDiseases.append(bDiseases, pet.diseases);
+    // liDiseases.innerHTML = '<b>Diseases: </b>' + pet.diseases;
+
+    const liParasites = document.createElement("li");
+    const bParasites = document.createElement("b");
+    bParasites.textContent = 'Parasites: ';
+    liParasites.append(bParasites, pet.parasites);
+
+    ulDesc.append(liAge, liInoculations, liDiseases, liParasites);
+
+    descriptionPet.append(petTitle, pBreed, pDescription, ulDesc);
+
+    popupContent.append(imgPopup, descriptionPet);
+
+    body.classList.toggle("shadow");
+}
+
 function createPet(pet){
     const card = document.createElement('div');
     card.classList.add("our-friends-slider-card");
@@ -39,67 +98,7 @@ function createPet(pet){
     card.append(name);
     card.append(btn);
 
-    card.addEventListener("click", (e) => {
-        popupCard.classList.toggle("hidden");
-        const popupContent = document.querySelector('.pop-up__content');
-        popupContent.textContent = '';
-
-        const imgPopup = document.createElement('img');
-        imgPopup.alt = '';
-        imgPopup.src = pet.img;
-
-        const descriptionPet = document.createElement('div');
-        descriptionPet.classList.add("description-pet");
-
-        const petTitle = document.createElement('h3');
-        petTitle.classList.add('pet-title');
-        petTitle.textContent = pet.name;
-
-        const pBreed = document.createElement('p');
-        pBreed.classList.add('breed');
-        pBreed.textContent = pet.type + " - " + pet.breed;
-
-        const pDescription = document.createElement('p');
-        pDescription.classList.add('description');
-        pDescription.textContent = pet.description;
-
-
-        const ulDesc = document.createElement("ul");
-        const liAge = document.createElement("li");
-        const bAge = document.createElement("b");
-        bAge.textContent = 'Age: ';
-        liAge.append(bAge, pet.age);
-
-        const liInoculations = document.createElement("li");
-        const bInoculations = document.createElement("b");
-        bInoculations.textContent = 'Inoculations: ';
-        liInoculations.append(bInoculations, pet.inoculations);
-
-        const liDiseases = document.createElement("li");
-        const bDiseases = document.createElement("b");
-        bDiseases.textContent = 'Diseases: ';
-        liDiseases.append(bDiseases, pet.diseases);
-        // liDiseases.innerHTML = '<b>Diseases: </b>' + pet.diseases;
-
-        const liParasites = document.createElement("li");
-        const bParasites = document.createElement("b");
-        bParasites.textContent = 'Parasites: ';
-        liParasites.append(bParasites, pet.parasites);
-
-        ulDesc.append(liAge);
-        ulDesc.append(liInoculations);
-        ulDesc.append(liDiseases);
-        ulDesc.append(liParasites);
-
-        descriptionPet.append(petTitle);
-        descriptionPet.append(pBreed);
-        descriptionPet.append(pDescription);
-        descriptionPet.append(ulDesc);
-
-        popupContent.append(imgPopup);
-        popupContent.append(descriptionPet);
-        body.classList.toggle("shadow");
-    });
+    card.addEventListener("click", (e) => fillPopup(pet));
 
     popupCard.addEventListener("click", popupClose);
 
@@ -174,13 +173,3 @@ BUTTON_RIGHT.addEventListener("click", newSliderPets);
 PETS_LIST_MAIN.addEventListener("animationend", () => {
     PETS_LIST_MAIN.classList.remove("transition-right");
 })
-
-petsCard.addEventListener("click", (e) => {
-    popupCard.classList.toggle("hidden");
-    body.classList.toggle("shadow");
-    // console.log('generatePetsOrder', generatePetsOrder());
-});
-
-popupCard.addEventListener("click", popupClose);
-
-
