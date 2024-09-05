0bf483734369c76b9ba319026973f360fcbaf109
Alina M <crlin.m@gmail.com>
2024-09-05 21:58:40 +0500

    feat: pets 1280px - generate random pets for pagination, refactor random functions, pets.html for pagination

--- shelter/src/js/pets.js
+++ shelter/src/js/pets.js
@@ -1,57 +1,92 @@
-import {getPets} from "./utils.js"
+import {getPets, generatePetsOrder} from "./utils.js"
 import {Pet} from './Pet.js';
 
+const BUTTON_LEFT2 = document.querySelector(".left-arrow2");
+const BUTTON_LEFT = document.querySelector(".left-arrow");
+const BUTTON_RIGHT = document.querySelector(".right-arrow");
+const BUTTON_RIGHT2 = document.querySelector(".right-arrow2");
+const PAGE_NUMBER = document.querySelector(".current-page");
 const PETS_LIST_MAIN = document.querySelector(".our-friends-pics-flex")
 
 let pets;
-let current = [];
-let previous;
 let rest;
+const pagesCnt = 8;
+let petsAll = new Array(pagesCnt);
+let currentPage = 0;
 
-function randomIndex(minInd, maxInd){
-    return Math.floor(Math.random()*(maxInd - minInd) + minInd);
-}
 
-function generatePetsOrderMain(cntCards, cntPets) {
-    let minInd = 0;
-    const maxInd = rest.length;
-
-    while (minInd < cntCards){
-        let randomInd = randomIndex(minInd, maxInd);
-        const temp = rest[minInd];
-        rest[minInd] = rest[randomInd];
-        rest[randomInd] = temp;
-        minInd++;
-    }
-
-    console.log('previous, current, rest before: ', previous, current, rest);
-    previous = current;
-    current = rest.slice(0, cntCards);
-    rest = rest.slice(cntCards, cntPets).concat(previous);
-    console.log('previous, current, rest after: ', previous, current, rest);
-}
-
-async function addRandomPets(startPos, cntCards){
+function addRandomPets(current, startPos, cntCards){
+    PETS_LIST_MAIN.textContent = '';
     for (let i of current.slice(startPos, cntCards)){
         const petCard = pets[i].createPet();
         PETS_LIST_MAIN.append(petCard);
     }
 }
 
+const generatePets = (data) => data.map(pet => new Pet(pet));
+
 async function init(){
     const data = await getPets();
     const cntPets = data.length;
     pets = generatePets(data);
 
-    const cntCards = 8;
     const startPos = 0;
-    rest = Array.from({length: cntPets}, (v, i) => i);
+    const cntCards = 8;
+    // rest = Array.from({length: cntPets}, (v, i) => i);
+    // rest = generatePetsOrder(rest, cntCards);
+    
+    for (let i=0; i < petsAll.length; i++){
+        rest = Array.from({length: cntPets}, (v, i) => i);
+        rest = generatePetsOrder(rest, cntCards);
+        petsAll[i] = rest;
+    }
+    
+    console.log(petsAll)
 
-    generatePetsOrderMain(cntCards, cntPets);
-
-    await addRandomPets(startPos, cntCards);
+    addRandomPets(petsAll[currentPage], startPos, cntCards);
 }
 
 init()
 
-const generatePets = (data) => data.map(pet => new Pet(pet));
+function buttonLeftPress(){
+    const startPos = 0;
+    const cntCards = 8;
+    currentPage--;
+    addRandomPets(petsAll[currentPage], startPos, cntCards);
+    PAGE_NUMBER.textContent = currentPage+1;
+    if (currentPage+1 == 1){
+        BUTTON_LEFT.classList.remove("next-item")
+        BUTTON_LEFT.classList.add("inactive-item")
+        BUTTON_LEFT2.classList.remove("next-item")
+        BUTTON_LEFT2.classList.add("inactive-item")
+    }
+    if (currentPage+1 < 8){
+        BUTTON_RIGHT.classList.add("next-item")
+        BUTTON_RIGHT.classList.remove("inactive-item")
+        BUTTON_RIGHT2.classList.add("next-item")
+        BUTTON_RIGHT2.classList.remove("inactive-item")
+    }
+}
+
+function buttonRightPress(){
+    const startPos = 0;
+    const cntCards = 8;
+    currentPage++;
+    addRandomPets(petsAll[currentPage], startPos, cntCards);
+    PAGE_NUMBER.textContent = currentPage+1;
+    if (currentPage+1 > 1){
+        BUTTON_LEFT.classList.add("next-item")
+        BUTTON_LEFT.classList.remove("inactive-item")
+        BUTTON_LEFT2.classList.add("next-item")
+        BUTTON_LEFT2.classList.remove("inactive-item")
+    }
+    if (currentPage+1 == pagesCnt){
+        BUTTON_RIGHT.classList.remove("next-item")
+        BUTTON_RIGHT.classList.add("inactive-item")
+        BUTTON_RIGHT2.classList.remove("next-item")
+        BUTTON_RIGHT2.classList.add("inactive-item")
+    }
+}
+
+BUTTON_LEFT.addEventListener("click", buttonLeftPress)
+BUTTON_RIGHT.addEventListener("click", buttonRightPress)
